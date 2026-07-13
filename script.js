62764b2ca62fd955158b673f857d8c192ae6a259
rugowl20-hash <rugowl20@gmail.com>
2026-07-13 13:09:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-async function submitOrderForm() {
-  // ... modify appState.orders
-  await saveOrders();   // new Firebase save
-  renderOrders();
+rules_version = '2';
+service cloud.firestore {
+  match /databases/{database}/documents {
+    match /{document=**} {
+      allow read, write: if true;
+    }
+  }
 }
